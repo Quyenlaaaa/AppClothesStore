03b583b77f7cf88a1b9fe8a8fca583bdb4dd7a2d
Tayeu9 <Tiensihohuynh123@gmail.com>
2023-11-19 13:25:31 +0700

    lan 1

--- app/src/main/java/com/example/app_banhangtructuyen/activity/MainActivity.java
+++ app/src/main/java/com/example/app_banhangtructuyen/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.app_banhangtructuyen.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,8 +8,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.app_banhangtructuyen.R;
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     TextView txtsignup;
@@ -18,6 +26,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AnhXa();
+        FirebaseDatabase firebase = FirebaseDatabase.getInstance();
+        DatabaseReference myRef= firebase.getReference("products");
+    myRef.addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for (DataSnapshot snap: snapshot.getChildren())
+                {
+                    String name =snap.getKey();
+                    Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+                }
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    });
         txtsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
