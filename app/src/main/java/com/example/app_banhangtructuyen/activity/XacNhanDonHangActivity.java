e4f231f752c2b33d2b18c3a5f61fb1d466e2af61
Quyenlaaaa <quyenlathe4@gmail.com>
2023-12-11 21:40:16 +0700

    update them dia chi

--- app/src/main/java/com/example/app_banhangtructuyen/activity/XacNhanDonHangActivity.java
+++ app/src/main/java/com/example/app_banhangtructuyen/activity/XacNhanDonHangActivity.java
@@ -1,5 +1,6 @@
 package com.example.app_banhangtructuyen.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,23 +17,56 @@ import com.example.app_banhangtructuyen.R;
 import com.example.app_banhangtructuyen.adapter.SanPhamInDatHangAdapter;
 import com.example.app_banhangtructuyen.adapter.SanphamAdapter;
 import com.example.app_banhangtructuyen.model.ItemCart;
-import com.example.app_banhangtructuyen.model.Product;
+import com.example.app_banhangtructuyen.model.UserSingleton;
+import com.example.app_banhangtructuyen.model.Users;
 import com.example.app_banhangtructuyen.model.ShoppingCart;
 import com.example.app_banhangtructuyen.model.ShoppingCartSingleton;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.List;
 
 public class XacNhanDonHangActivity extends AppCompatActivity {
     ShoppingCart shoppingCart;
     double tongtien;
     double tongtien2;
-    TextView tt, tt2, tt3;
+    TextView tt, tt2, tt3, tenuser, sodt, diachi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xac_nhan_don_hang);
+        tenuser = findViewById(R.id.tenuser);
+        sodt = findViewById(R.id.sodt);
+        diachi= findViewById(R.id.diachi);
+        UserSingleton userSingleton = UserSingleton.getInstance();
+        String email = userSingleton.getUsername();
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = firebaseDatabase.getReference("Khachhang");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot snap:snapshot.getChildren())
+                {
+                    String emaila = snap.child("phone_email").getValue(String.class);
+
+                    if( email.equals(emaila)==true)
+                    {
+                        tenuser.setText(snap.child("hoTen").getValue(String.class));
+                        sodt.setText(snap.child("sodt").getValue(String.class));
+                        diachi.setText(snap.child("diaChi").getValue(String.class));
+                    }
+                } 
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
         shoppingCart = ShoppingCartSingleton.getInstance().getShoppingCart();
         ShowListSPDM();
         tongtien = shoppingCart.getCountItemCart();
@@ -51,11 +85,6 @@ public class XacNhanDonHangActivity extends AppCompatActivity {
     private void ShowListSPDM() {
         ListView listView = findViewById(R.id.lvquanao);
         List<ItemCart> arrayList = shoppingCart.getCartItems();
-
-//        arrayList.add(new Product(1,"Áo khoác nam",150000,R.drawable.sanpham," ",1));
-//        arrayList.add(new Product(2,"Áo khoác nam",150000,R.drawable.sanpham," ",2));
-//        arrayList.add(new Product(3,"Áo khoác nam",150000,R.drawable.sanpham," ",3));
-//        arrayList.add(new Product(4,"Áo khoác nam",150000,R.drawable.sanpham," ",4));
         SanPhamInDatHangAdapter adapter = new SanPhamInDatHangAdapter(this,arrayList);
         listView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
